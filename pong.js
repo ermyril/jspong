1826cfe94e4e14a4c7776192dcdadabc78b8d632
Er Myril <ErMyril@gmail.com>
2020-06-25 20:09:14 +0300

    добавили платформу для игрока

--- pong.js
+++ pong.js
@@ -7,6 +7,14 @@ class Color {
     }
 }
 
+
+class Position {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
 class Frame {
     constructor(imageData) {
         this.imageData = imageData;
@@ -28,11 +36,74 @@ class Frame {
 
 }
 
+class Paddle {
+    constructor( pos, width, height, velocity, color, pongConfig ) {
+        this.pongConfig = pongConfig;
+        this.width = width;
+        this.height = height;
+        this.velocity = velocity;
+        this.color = color;
+
+        this.movingUp = false;
+        this.movingDown = false;
+
+        this.pos = new Position(
+            pos.x - width / 2,
+            pos.y - height / 2
+        );
+
+        this.registerHandlers();
+
+    }
+
+    draw(frame) {
+
+
+
+        for (let y = 0; y < this.height; y++ ) {
+            for (let x = 0; x < this.width; x++) {
+                frame.setPixel(this.pos.x + x, this.pos.y + y, this.color, frame.pixels);
+            }
+        }
+    }
+
+    update() {
+        if (this.movingDown && (this.pos.y + this.height) < this.pongConfig.height) {
+            this.pos.y += this.velocity;
+        } else if (this.movingUp && this.pos.y > 0) {
+            this.pos.y -= this.velocity;
+        }
+    }
+
+    registerHandlers() {
+        document.addEventListener("keydown", event => {
+            if (event.code === "ArrowDown") {
+                this.movingDown = true;
+            } else if (event.code === "ArrowUp") {
+                this.movingUp = true;
+            }
+        });
+
+        document.addEventListener("keyup", event => {
+            if (event.code === "ArrowDown") {
+                this.movingDown = false;
+            } else if (event.code === "ArrowUp") {
+                this.movingUp = false;
+            }
+        });
+    }
+}
+
 class Pong {
     constructor(canvas, width, height) {
+        this.config = {
+            width,
+            height
+        }
+
         this.canvas = canvas;
-        this.canvas.width = width;
-        this.canvas.height = height;
+        this.canvas.width = this.config.width;
+        this.canvas.height = this.config.height;
         this.canvas.style = "border: 3px solid #bababa";
         this.lastRender = 0;
 
@@ -40,6 +111,14 @@ class Pong {
     }
 
     start() {
+        this.player1 = new Paddle(
+            new Position(20, this.config.height / 2),
+            10, // width
+            60, // height
+            5,  // velocity
+            new Color(0, 0, 0, 255),
+            this.config // pongConfig
+        );
 
         window.requestAnimationFrame(this.loop);
     }
@@ -48,26 +127,22 @@ class Pong {
 
         let progress = timestamp - this.lastRender;
 
-        this.draw();
+        this.render();
 
         this.lastRender = timestamp;
 
         window.requestAnimationFrame(this.loop);
     }
 
-    draw() {
+    render() {
         let imageData = this.ctx.createImageData(this.canvas.width, this.canvas.height);
 
         const frame = new Frame(imageData);
+
+
+        this.player1.update();
+        this.player1.draw(frame);
        
-
-        for (let w = 0; w < 640; w++) {
-            for (let h = 0; h < 480; h++ ) {
-                //frame.setPixel(w, h, new Color(0, 0, 0, w/2));
-                frame.setPixel(w, h, new Color(0, 0, 0, (w^h)));
-            }
-        }
-
 
         this.ctx.putImageData(frame.imageData, 0, 0);
     }
